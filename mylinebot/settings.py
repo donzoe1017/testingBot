21f420a4e552dd27883e82abca88a057716300f3
bubblec at <york22731858@gmail.com>
2022-01-18 02:26:59 +0800

    attempt to connect to mongodb

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-gnr3a+m9zefz4pi5hwojrn*v&8hr8@bdw^u=7inlhkjl0j23jt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['f0a0-118-163-176-235.ngrok.io']
-#ALLOWED_HOSTS = ['game-in-line-webpage.herokuapp.com']
+#ALLOWED_HOSTS = ['f0a0-118-163-176-235.ngrok.io']
+ALLOWED_HOSTS = ['game-in-line-webpage.herokuapp.com']
 
 #LINE_ACCESS_TOKEN
 LINE_CHANNEL_ACCESS_TOKEN = 'c+zxxPnYV2LhC5nPazqSAgijtfrfTILEcXLP7ug49RUGm3JW78YrLFcLKxJfpEU1juSZAbgzu2sLo5+/VqFt7k2f8oJOHpddtmAmkvVgBMA2x3YodM/AMcXZlinUKP2PdMHxrLicxRnVFnHOPu3IyQdB04t89/1O/w1cDnyilFU='
@@ -82,12 +82,23 @@ WSGI_APPLICATION = 'mylinebot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'linebot',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'mongodb+srv://donzoe1017:cxz99856@cluster0.t5qzo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+            }  
+        }
 }
 
 
