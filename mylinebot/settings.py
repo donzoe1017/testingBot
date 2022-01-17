ac2d09fb1bb134406b5df2b6ac3186aacfdf72df
bubblec at <york22731858@gmail.com>
2022-01-17 16:59:33 +0800

    second attempt to init

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -25,15 +25,15 @@ SECRET_KEY = 'django-insecure-gnr3a+m9zefz4pi5hwojrn*v&8hr8@bdw^u=7inlhkjl0j23jt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '7cce-59-125-27-110.ngrok.io'
-]
+#ALLOWED_HOSTS = ['7cce-59-125-27-110.ngrok.io']
+ALLOWED_HOSTS = ['game-in-line-webpage.herokuapp.com']
 
 #LINE_ACCESS_TOKEN
 LINE_CHANNEL_ACCESS_TOKEN = 'c+zxxPnYV2LhC5nPazqSAgijtfrfTILEcXLP7ug49RUGm3JW78YrLFcLKxJfpEU1juSZAbgzu2sLo5+/VqFt7k2f8oJOHpddtmAmkvVgBMA2x3YodM/AMcXZlinUKP2PdMHxrLicxRnVFnHOPu3IyQdB04t89/1O/w1cDnyilFU='
 
 LINE_CHANNEL_SECRET = '8c2a6b9a55f399c320a6f8e1932fc0c9'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
