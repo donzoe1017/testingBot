637b2072949ac89183bd380d156510c1a73766a3
bubblec at <york22731858@gmail.com>
2022-10-19 21:39:30 +0800

    modify manage.py

--- manage.py
+++ manage.py
@@ -3,27 +3,6 @@
 import os
 import sys
 
-from http import server
-from http.server import SimpleHTTPRequestHandler
-import socket
-import ssl
-import sys
-
-
-if sys.argv[1:]:
-        port = int(sys.argv[1])
-else :
-        port = 8000
-
-server_address = ("127.0.0.1", port)
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-#context.load_cert_chain("xxx.pem","xxx.key")#自己新增
-
-httpd = server.HTTPServer(server_address,SimpleHTTPRequestHandler)
-httpd.socket = context.wrap_socket(httpd.socket, server_side = True)
-httpd.serve_forever()
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylinebot.settings')
